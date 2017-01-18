352dac50b073f6971d692b73179f4924c88f62ea
Lucas <Lucas@10.250.19.142>
2017-01-18 12:44:46 -0500

    Added card and deck of cards classes

---
+++ src/week2githubintegration/DeckOfCards.java
@@ -0,0 +1,61 @@
+package week2githubintegration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author Lucas
+ */
+public class DeckOfCards {
+    private ArrayList<Card> deck;
+    
+    public DeckOfCards ()
+    {
+        deck= new ArrayList<>();
+        String[] suits = {"hearts","clubs","spades","diamonds"};
+        String[] faceNames = {"two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace"};
+        
+        // passa cada valor de suit pra dentro de suit
+        for(String suit: suits)
+        {
+         for(int i=0; i < faceNames.length; i++)
+         {
+             Card newCard = new Card(faceNames[i],suit, i+2);
+             deck.add(newCard);
+         }
+        }
+    }
+    
+    /**
+     * This will return a Sting representing the eck of cards
+     */
+   
+    @Override
+    public String toString()
+    {
+        String deckString = "";
+        for(Card card : deck)
+        {
+            deckString += card.toString() + "\n";
+        }
+        return deckString;
+    }
+    /**
+     * This method will shuffle the deck of cards
+     */
+    public void shuffle(){
+        Collections.shuffle(deck);
+    }
+    /**
+     * This method will deal the top card off the deck
+     */
+    
+    public Card dealTopCard ()
+    {
+        if (deck.isEmpty())
+            return null;
+        else
+            return deck.remove(0);
+    }
+}

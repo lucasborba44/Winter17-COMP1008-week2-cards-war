d11f65a0d1a9189fcf0f3029f11179008bb6b16e
Lucas <Lucas@10.250.19.142>
2017-01-18 12:38:00 -0500

    Initial submission

--- src/week2githubintegration/Week2githubintegration.java
+++ src/week2githubintegration/Week2githubintegration.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package week2githubintegration;
 
 /**
  *
- * @author Lucas
+ * @author Lucas Borba 200352679
  */
 public class Week2githubintegration {
 
